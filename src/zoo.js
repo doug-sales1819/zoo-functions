04f285d941cca3526ca1aadf48662b0db66759c8
Douglas Sales <douglas.sales0611@gmail.com>
2023-04-02 22:45:51 -0300

    ✨ feat(zoo.js): requisito 2 feito
    implementar a função animalsOlderThan

--- src/zoo.js
+++ src/zoo.js
@@ -1,14 +1,3 @@
-/*
-eslint no-unused-vars: [
-  "error",
-  {
-    "args": "none",
-    "vars": "local",
-    "varsIgnorePattern": "data"
-  }
-]
-*/
-
 const data = require('./data');
 
 const {
@@ -23,7 +12,24 @@ const {
   - Esta função é responsável pela busca das espécies de animais por id
   - Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro,
   - podendo receber um ou mais ids.
-  @param id
+  @param ...ids: string - required
+  return [{}] | []
 */
 const animalsByIds = (...ids) => animals.filter(({ id }) => ids.includes(id));
 
+/*
+  TODO: 2. IMPLEMENTE A FUNÇÃO animalsOlderThan
+  - Esta função, a partir do nome de uma espécie e uma idade mínima,
+  - verifica se todos os animais daquela espécie possuem a idade mínima especificada
+  @param species: string - required
+  @param minAge: number - required
+  return boolean
+*/
+const animalsOlderThan = (species, minAge) => (
+  animals.find((animal) => animal.name === species).residents.every(({ age }) => age >= minAge)
+);
+
+module.exports = {
+  animalsByIds,
+  animalsOlderThan,
+};
