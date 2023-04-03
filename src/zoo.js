6364e63d1af23f4212c1b4af1bc30bdf45dd456b
Douglas Sales <douglas.sales0611@gmail.com>
2023-04-02 22:21:04 -0300

    ✨ feat(zoo.js): requisito 1 feito
    requisito de implementar A função animalsByIds

--- src/zoo.js
+++ src/zoo.js
@@ -11,8 +11,19 @@ eslint no-unused-vars: [
 
 const data = require('./data');
 
-const { animals, employees, hours, prices } = data;
+const {
+  animals,
+  employees,
+  hours,
+  prices,
+} = data;
 
-
-
+/*
+  TODO: 1. IMPLEMENTE A FUNÇÃO animalsByIds
+  - Esta função é responsável pela busca das espécies de animais por id
+  - Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro,
+  - podendo receber um ou mais ids.
+  @param id
+*/
+const animalsByIds = (...ids) => animals.filter(({ id }) => ids.includes(id));
 
