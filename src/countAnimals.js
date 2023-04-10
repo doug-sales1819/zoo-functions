cc52169d27f190dbaf7f8072f526782c589d9646
Douglas Sales <douglas.sales0611@gmail.com>
2023-04-10 12:19:37 -0300

    refatora o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,34 +1,17 @@
 const { species: animals } = require('../data/zoo_data');
 
-const countSpeciesBySex = (residents, targetSex) => (
-  residents.filter(({ sex }) => sex === targetSex).length
-);
-
-const getCountOfAllSpecies = () => (
-  animals.reduce((acc, { name, residents }) => {
-    acc[name] = residents.length;
+const getSpeciesCount = ({ species, sex }) => {
+  const animalCount = animals.reduce((acc, { name, residents }) => {
+    acc[name] = !sex ? residents.length : residents.filter((specie) => specie.sex === sex).length;
     return acc;
-  }, {})
-);
+  }, {});
+  return !species ? animalCount : animalCount[species];
+};
 
-const countAnimals = (specie) => {
+const countAnimals = (options) => {
   const animal = { species: '', sex: '' };
-
-  Object.assign(animal, specie);
-
-  const speciesCount = getCountOfAllSpecies();
-
-  if (!animal.species) {
-    return speciesCount;
-  }
-  if (animal.species && animal.sex) {
-    const matchingSpecie = animals.find(({ name }) => name === animal.species);
-    if (matchingSpecie) {
-      return countSpeciesBySex(matchingSpecie.residents, animal.sex);
-    }
-    return 0;
-  }
-  return speciesCount[animal.species];
+  Object.assign(animal, options);
+  return getSpeciesCount(animal);
 };
 
 module.exports = countAnimals;
